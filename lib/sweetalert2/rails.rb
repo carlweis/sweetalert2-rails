b576b8bfce83c9e42bf9f9fc7419e99e5af83136
Carl Weis <carl@givecampus.com>
2017-04-30 16:20:25 -0400

    Initialize gem

---
+++ lib/sweetalert2/rails.rb
@@ -0,0 +1,7 @@
+require "sweetalert2/rails/version"
+
+module Sweetalert2
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ spec/sweetalert2/rails_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Sweetalert2::Rails do
+  it "has a version number" do
+    expect(Sweetalert2::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
